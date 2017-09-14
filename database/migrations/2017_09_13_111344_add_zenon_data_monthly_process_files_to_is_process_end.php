64cddbb96c419ff1b4ad537d35dcac6b6401ebb9
r-kawanishi <r-kawanishi@A576PX-Dev02.sinren_net.local>
2017-09-14 10:28:16 +0900

    migrationファイルの修正

--- database/migrations/2017_09_13_111344_add_zenon_data_monthly_process_files_to_is_process_end.php
+++ database/migrations/2017_09_13_111344_add_zenon_data_monthly_process_files_to_is_process_end.php
@@ -12,6 +12,23 @@ class AddZenonDataMonthlyProcessFilesToIsProcessEnd extends Migration
     public function up() {
         Schema::connection($this->connect)->table($this->tableName, function(Blueprint $table) {
 
+
+            if (!Schema::connection($this->connect)->hasColumn($this->tableName, 'is_pre_process'))
+            {
+                $table->boolean('is_pre_process')
+                        ->after('zenon_data_csv_file_id')
+                ;
+            }
+
+            if (!Schema::connection($this->connect)->hasColumn($this->tableName, 'is_post_process'))
+            {
+                $table->boolean('is_post_process')
+                        ->after('is_pre_process')
+                ;
+            }
+
+
+
             if (!Schema::connection($this->connect)->hasColumn($this->tableName, 'is_process_end'))
             {
                 $table->boolean('is_process_end')
@@ -33,6 +50,18 @@ class AddZenonDataMonthlyProcessFilesToIsProcessEnd extends Migration
     }
 
     public function down() {
+        if (Schema::connection($this->connect)->hasColumn($this->tableName, 'is_pre_process'))
+        {
+            Schema::connection($this->connect)->table($this->tableName, function(Blueprint $table) {
+                $table->dropColumn('is_pre_process');
+            });
+        }
+        if (Schema::connection($this->connect)->hasColumn($this->tableName, 'is_post_process'))
+        {
+            Schema::connection($this->connect)->table($this->tableName, function(Blueprint $table) {
+                $table->dropColumn('is_post_process');
+            });
+        }
         if (Schema::connection($this->connect)->hasColumn($this->tableName, 'is_process_end'))
         {
             Schema::connection($this->connect)->table($this->tableName, function(Blueprint $table) {
