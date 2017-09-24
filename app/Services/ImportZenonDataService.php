4ed3c4d823c2954827af16740807aaa4967586a0
vagrant <vagrant@cvs>
2017-09-24 23:44:15 +0900

    データベースセットサービスの修正

--- app/Services/ImportZenonDataService.php
+++ app/Services/ImportZenonDataService.php
@@ -4,75 +4,133 @@ namespace App\Services;
 
 use \App\Services\Traits\JsonUsable;
 use \App\Services\Traits\CsvUsable;
+use \App\Services\Traits\TypeConvertable;
 
 class ImportZenonDataService
 {
 
     use JsonUsable,
-        CsvUsable;
+        CsvUsable,
+        TypeConvertable;
 
-    public function getCsvSplitRow($csv_file_object, $table_columns, $table_types, $table_config, $month_id) {
-        mb_convert_variables('UTF-8', 'SJIS-WIN', $csv_file_object);
-        $convert_row = [];
-        foreach ($row as $i => $buf) {
-            $convert_row[] = $this->convertValue($buf, $table_types[$i]);
-        }
-        $convert_row       = array_combine($table_columns, $convert_row);
-        $convert_row['id'] = null;
+    protected $row;
 
-        if ($table_config->is_cumulative == true)
+    public function getRow() {
+        return $this->row;
+    }
+
+    public function setRow($row) {
+        $this->row = $row;
+//        var_dump($row);
+        return $this;
+    }
+
+    public function setTimeStamp($timestamp = null) {
+        if (empty($timestamp))
         {
-            $convert_row['monthly_id'] = $month_id;
+            $timestamp = date('Y-m-d H:i:s');
         }
-        if ($table_config->is_account_convert == true)
+        $this->row['created_at'] = $timestamp;
+        $this->row['updated_at'] = $timestamp;
+        return $this;
+    }
+
+    public function convertRow($types, $is_ceil = true) {
+//        var_dump($this->row);
+        $row       = $this->convertTypes($types, $this->row, $is_ceil);
+//        var_dump($row);
+        $this->row = $row;
+        return $this;
+    }
+
+    public function setKeyToRow($keys) {
+        $row       = array_combine($keys, $this->row);
+        $row['id'] = null;
+        $this->row = $row;
+        return $this;
+    }
+
+    public function setMonthlyIdToRow($is_cumulative, $monthly_id = null) {
+        if (!$is_cumulative)
         {
-            $p = [
-                'account' => $table_config->account_column_name,
-                'subject' => $table_config->subject_column_name,
-            ];
-            if ($p['account'] == '' || $p['account'] == '')
-            {
-                throw new \Exception("subject_column_nameもしくはaccount_column_nameの値が不正です。");
-            }
-            $convert_row['key_account_number'] = $this->convertAccount($p, $convert_row);
+            return $this;
+        }
+        if (empty($monthly_id))
+        {
+            throw new \Exception('月別IDが指定されていません。');
+        }
+        $this->row['monthly_id'] = $monthly_id;
+        return $this;
+    }
+
+    public function setConvertedAccountToRow($is_account_convert,$account_convert_param = null) {
+        if (!$is_account_convert)
+        {
+            return $this;
+        }
+        if (empty($account_convert_param))
+        {
+            throw new \Exception('口座分割設定値が指定されていません。');
+        }
+//        var_dump($account_convert_param);
+        if (
+                empty($account_convert_param['account_column_name']) ||
+                empty($account_convert_param['subject_column_name'])
+        )
+        {
+            throw new \Exception("指定された口座番号変換キーが不正です。");
+        }
+//        var_dump($this->row);
+        $this->row['key_account_number'] = (int) $this->convertAccount($account_convert_param, $this->row);
+        return $this;
+    }
+
+    public function splitRow($is_split, $pos_first = 0, $pos_last = 0, $split_key_configs = null) {
+        if (!$is_split)
+        {
+            return $this;
         }
 
-        $convert_row['created_at'] = date("Y-m-d H:i:s");
-        $convert_row['updated_at'] = date("Y-m-d H:i:s");
-        if (!$table_config->is_split)
+        if (!isset($pos_first) || $pos_first < 0)
         {
-            return $convert_row;
+            throw new \Exception("配列切り落としの開始位置が誤っているようです。");
         }
-
-        // 切り落とし処理
-        $first     = $table_config->first_column_position;
-        $last      = $table_config->last_column_position;
-        $slice_row = array_slice($convert_row, $first, $last, true);
+        if (!isset($pos_last) || $pos_last < 0)
+        {
+            throw new \Exception("配列切り落としの終了位置が誤っているようです。");
+        }
+        if ($pos_first >= $pos_last)
+        {
+            throw new \Exception("配列切り落としの指定は開始位置 < 終了位置となるように指定してください。");
+        }
+        $slice_row = array_slice($this->row, $pos_first, $pos_last, true);
         for ($i = 1; $i <= 4; $i++) {
-            $k  = 'split_foreign_key_' . $i;
-            $fk = $table_config->$k;
-            if ($fk != null)
+            $k = 'split_foreign_key_' . $i;
+            if (!empty($split_key_configs[$k]))
             {
-                $slice_row[$fk] = $convert_row[$fk];
+                $fk             = $split_key_configs[$k];
+                $slice_row[$fk] = $this->row[$fk];
             }
         }
-        if ($table_config->is_cumulative == true)
-        {
-            $slice_row['monthly_id'] = $convert_row['monthly_id'];
-        }
-        if ($table_config->is_account_convert == true)
-        {
-            $slice_row['key_account_number'] = $convert_row['key_account_number'];
-        }
-        $slice_row['created_at'] = $convert_row['created_at'];
-        $slice_row['updated_at'] = date("Y-m-d H:i:s");
-        return $slice_row;
+//        if ($split_key_configs->is_cumulative == true)
+//        {
+//            $slice_row['monthly_id'] = $this->row['monthly_id'];
+//        }
+//        if ($split_key_configs->is_account_convert == true)
+//        {
+//            $slice_row['key_account_number'] = $this->row['key_account_number'];
+//        }
+
+        $this->row = $slice_row;
+        return $this;
     }
 
     private function convertAccount($param, $row) {
 //        echo "sfdfsd";
-        $ac_key = $param['account'];
-        $sb_key = $param['subject'];
+//        $ac_key = $param['account'];
+//        $sb_key = $param['subject'];
+        $ac_key = $param['account_column_name'];
+        $sb_key = $param['subject_column_name'];
         switch ($row[$sb_key]) {
             case 1:
             case 2:
@@ -90,46 +148,103 @@ class ImportZenonDataService
         }
     }
 
-    private function convertValue($buf, $attr) {
-        switch ($attr) {
-            case 'char':
-                return $this->splitSpace($buf);
-
-            case 'integer':
-            case 'bigInteger':
-                if (!is_numeric($buf) && $buf != ' ' && $buf != '')
-                {
-                    throw new \Exception("数値型に変換できませんでした（値：[{$buf}]）");
-                }
-                return (int) $buf;
-            case 'float':
-            case 'double':
-                if (!is_numeric($buf) && $buf != ' ' && $buf != '')
-                {
-                    throw new \Exception("数値型に変換できませんでした（値：[{$buf}]）");
-                }
-                return (double) $buf;
-            case 'date':
-                return $this->convertDate($buf);
-            case 'boolean':
-                return (bool) $buf;
-            Default:
-                return $buf;
-        }
-    }
-
-    private function splitSpace($buf) {
-        $return = preg_replace("/^[\s　]*(.*?)[\s　]*$/u", "$1", $buf);
-        return $return;
-    }
-
-    private function convertDate($buf) {
-        if ($buf == '00000000')
-        {
-            return null;
-        }
-        return $buf;
-    }
+//    public function getCsvSplitRow($csv_file_object, $table_columns, $table_types, $table_config, $month_id) {
+//        mb_convert_variables('UTF-8', 'SJIS-WIN', $csv_file_object);
+//        $convert_row = [];
+//        foreach ($row as $i => $buf) {
+//            $convert_row[] = $this->convertValue($buf, $table_types[$i]);
+//        }
+//        $convert_row       = array_combine($table_columns, $convert_row);
+//        $convert_row['id'] = null;
+//
+//        if ($table_config->is_cumulative == true)
+//        {
+//            $convert_row['monthly_id'] = $month_id;
+//        }
+//        if ($table_config->is_account_convert == true)
+//        {
+//            $p = [
+//                'account' => $table_config->account_column_name,
+//                'subject' => $table_config->subject_column_name,
+//            ];
+//            if ($p['account'] == '' || $p['account'] == '')
+//            {
+//                throw new \Exception("subject_column_nameもしくはaccount_column_nameの値が不正です。");
+//            }
+//            $convert_row['key_account_number'] = $this->convertAccount($p, $convert_row);
+//        }
+//
+//        $convert_row['created_at'] = date("Y-m-d H:i:s");
+//        $convert_row['updated_at'] = date("Y-m-d H:i:s");
+//        if (!$table_config->is_split)
+//        {
+//            return $convert_row;
+//        }
+//
+//        // 切り落とし処理
+//        $first     = $table_config->first_column_position;
+//        $last      = $table_config->last_column_position;
+//        $slice_row = array_slice($convert_row, $first, $last, true);
+//        for ($i = 1; $i <= 4; $i++) {
+//            $k  = 'split_foreign_key_' . $i;
+//            $fk = $table_config->$k;
+//            if ($fk != null)
+//            {
+//                $slice_row[$fk] = $convert_row[$fk];
+//            }
+//        }
+//        if ($table_config->is_cumulative == true)
+//        {
+//            $slice_row['monthly_id'] = $convert_row['monthly_id'];
+//        }
+//        if ($table_config->is_account_convert == true)
+//        {
+//            $slice_row['key_account_number'] = $convert_row['key_account_number'];
+//        }
+//        $slice_row['created_at'] = $convert_row['created_at'];
+//        $slice_row['updated_at'] = date("Y-m-d H:i:s");
+//        return $slice_row;
+//    }
+//    private function convertValue($buf, $attr) {
+//        switch ($attr) {
+//            case 'char':
+//                return $this->splitSpace($buf);
+//
+//            case 'integer':
+//            case 'bigInteger':
+//                if (!is_numeric($buf) && $buf != ' ' && $buf != '')
+//                {
+//                    throw new \Exception("数値型に変換できませんでした（値：[{$buf}]）");
+//                }
+//                return (int) $buf;
+//            case 'float':
+//            case 'double':
+//                if (!is_numeric($buf) && $buf != ' ' && $buf != '')
+//                {
+//                    throw new \Exception("数値型に変換できませんでした（値：[{$buf}]）");
+//                }
+//                return (double) $buf;
+//            case 'date':
+//                return $this->convertDate($buf);
+//            case 'boolean':
+//                return (bool) $buf;
+//            Default:
+//                return $buf;
+//        }
+//    }
+//
+//    private function splitSpace($buf) {
+//        $return = preg_replace("/^[\s　]*(.*?)[\s　]*$/u", "$1", $buf);
+//        return $return;
+//    }
+//
+//    private function convertDate($buf) {
+//        if ($buf == '00000000')
+//        {
+//            return null;
+//        }
+//        return $buf;
+//    }
 
     public function monthlyStatus($ym, $processes) {
         $rows = \App\ZenonMonthlyStatus::month($ym)
@@ -145,107 +260,105 @@ class ImportZenonDataService
         return $rows;
     }
 
-    public function uploadToDatabase($r, $csv, $ym) {
-        $table            = \DB::connection('mysql_zenon')->table($r->table_name);
-        $table_column_obj = \App\ZenonTable::where(['zenon_format_id' => $r->zenon_format_id])->get(['column_name', 'column_type']);
-        $table_columns    = [];
-        $table_types      = [];
-
-        foreach ($table_column_obj as $t) {
-            $table_columns[] = $t->column_name;
-            $table_types[]   = $t->column_type;
-        }
-        $bulk_rows    = [];
-        $bulk_counter = 0;
-
-        $executed_rows = 0;
-        foreach ($csv as $i => $row) {
-            if ($row === [null])
-            {
-                continue;
-            }
-            else
-            {
-                $executed_rows++;
-            }
-
-            $tmp_bulk    = $this->setCsvSplitRow($row, $table_columns, $table_types, $r, $ym);
-            $cnt         = count($tmp_bulk);
-            $bulk_rows[] = $tmp_bulk;
-            // MySQLのバージョンによってはプリペアドステートメントが65536までに制限されているため、動的にしきい値を設ける
-            if ($i !== 0 && ($cnt + $bulk_counter) > 65000)
-            {
-                $r->executed_row_count = $executed_rows;
-                $r->save();
-
-                $table->insert($bulk_rows);
-                $bulk_counter = 0;
-                $bulk_rows    = [];
-            }
-            else
-            {
-                $bulk_counter += $cnt;
-            }
-        }
-        if ($bulk_rows !== [null])
-        {
-            $r->executed_row_count = $executed_rows;
-            $r->save();
-            $table->insert($bulk_rows);
-        }
-    }
-
-    public function getLastTraded($row) {
-        $date = $row->reference_last_traded_on;
-        if (empty($date) || $date == '0000-00-00')
-        {
-            return $row->last_traded_on;
-        }
-        return $date;
-    }
-
-    public function convertTypes($types, $rows) {
-        $tmp_rows = [];
-        foreach ($rows as $i => $row) {
-            foreach ($row as $key => $column) {
-
-                if (!array_key_exists($key, $types))
-                {
-//                    echo "notexist {$key}<br>";
-                    $tmp_rows[$i][$key] = $column;
-                    continue;
-                }
-
-                if ($types[$key] === 'integer')
-                {
-                    $tmp_rows[$i][$key] = (int) $column;
-                }
-                elseif ($types[$key] === 'float')
-                {
-                    $tmp_rows[$i][$key] = (float) $column;
-                }
-                elseif ($types[$key] === 'time')
-                {
-                    if ($column == 'NULL' || $column == 'null')
-                    {
-                        $tmp_rows[$i][$key] = null;
-                    }
-                    else
-                    {
-                        $tmp_rows[$i][$key] = date("H:i:s", strtotime($column));
-                    }
-                }
-                elseif ($types[$key] === 'boolean')
-                {
-                    $tmp_rows[$i][$key] = (bool) $column;
-                }
-                else
-                {
-                    $tmp_rows[$i][$key] = $column;
-                }
-            }
-        }
-        return $tmp_rows;
-    }
-
+//    public function uploadToDatabase($r, $csv, $ym) {
+//        $table            = \DB::connection('mysql_zenon')->table($r->table_name);
+//        $table_column_obj = \App\ZenonTable::where(['zenon_format_id' => $r->zenon_format_id])->get(['column_name', 'column_type']);
+//        $table_columns    = [];
+//        $table_types      = [];
+//
+//        foreach ($table_column_obj as $t) {
+//            $table_columns[] = $t->column_name;
+//            $table_types[]   = $t->column_type;
+//        }
+//        $bulk_rows    = [];
+//        $bulk_counter = 0;
+//
+//        $executed_rows = 0;
+//        foreach ($csv as $i => $row) {
+//            if ($row === [null])
+//            {
+//                continue;
+//            }
+//            else
+//            {
+//                $executed_rows++;
+//            }
+//
+//            $tmp_bulk    = $this->setCsvSplitRow($row, $table_columns, $table_types, $r, $ym);
+//            $cnt         = count($tmp_bulk);
+//            $bulk_rows[] = $tmp_bulk;
+//            // MySQLのバージョンによってはプリペアドステートメントが65536までに制限されているため、動的にしきい値を設ける
+//            if ($i !== 0 && ($cnt + $bulk_counter) > 65000)
+//            {
+//                $r->executed_row_count = $executed_rows;
+//                $r->save();
+//
+//                $table->insert($bulk_rows);
+//                $bulk_counter = 0;
+//                $bulk_rows    = [];
+//            }
+//            else
+//            {
+//                $bulk_counter += $cnt;
+//            }
+//        }
+//        if ($bulk_rows !== [null])
+//        {
+//            $r->executed_row_count = $executed_rows;
+//            $r->save();
+//            $table->insert($bulk_rows);
+//        }
+//    }
+//
+//    public function getLastTraded($row) {
+//        $date = $row->reference_last_traded_on;
+//        if (empty($date) || $date == '0000-00-00')
+//        {
+//            return $row->last_traded_on;
+//        }
+//        return $date;
+//    }
+//    public function convertTypes($types, $rows) {
+//        $tmp_rows = [];
+//        foreach ($rows as $i => $row) {
+//            foreach ($row as $key => $column) {
+//
+//                if (!array_key_exists($key, $types))
+//                {
+////                    echo "notexist {$key}<br>";
+//                    $tmp_rows[$i][$key] = $column;
+//                    continue;
+//                }
+//
+//                if ($types[$key] === 'integer')
+//                {
+//                    $tmp_rows[$i][$key] = (int) $column;
+//                }
+//                elseif ($types[$key] === 'float')
+//                {
+//                    $tmp_rows[$i][$key] = (float) $column;
+//                }
+//                elseif ($types[$key] === 'time')
+//                {
+//                    if ($column == 'NULL' || $column == 'null')
+//                    {
+//                        $tmp_rows[$i][$key] = null;
+//                    }
+//                    else
+//                    {
+//                        $tmp_rows[$i][$key] = date("H:i:s", strtotime($column));
+//                    }
+//                }
+//                elseif ($types[$key] === 'boolean')
+//                {
+//                    $tmp_rows[$i][$key] = (bool) $column;
+//                }
+//                else
+//                {
+//                    $tmp_rows[$i][$key] = $column;
+//                }
+//            }
+//        }
+//        return $tmp_rows;
+//    }
 }
--- tests/UnitImportZenonDataServiceTest.php
+++ tests/UnitImportZenonDataServiceTest.php
@@ -34,8 +34,8 @@ class UnitImportZenonDataServiceTest extends TestCase
      */
     public function 異常系_口座番号変換時エラー() {
         $param = [
-            'account' => 'account_number',
-            'subject' => 'subject_code',
+            'account_column_name' => 'account_number',
+            'subject_column_name' => 'subject_code',
         ];
         $rows  = [
             'account_number' => '1',
@@ -48,6 +48,33 @@ class UnitImportZenonDataServiceTest extends TestCase
         } catch (\Exception $e) {
             $this->assertEquals("口座番号が短すぎるようです（科目：1， 口座番号：1）", $e->getMessage());
         }
+
+        try {
+            $this->s->setConvertedAccountToRow(true);
+            $this->fail('例外発生なし');
+        } catch (\Exception $e) {
+            $this->assertEquals("口座分割設定値が指定されていません。", $e->getMessage());
+        }
+
+        try {
+            $this->s->setConvertedAccountToRow(true, ['account_column_name' => '', 'subject_column_name' => '',]);
+            $this->fail('例外発生なし');
+        } catch (\Exception $e) {
+            $this->assertEquals("指定された口座番号変換キーが不正です。", $e->getMessage());
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function 異常系_月別IDセット処理() {
+        $row = ['1', '1234567890', 'key_1', 'test user_1', '20170701', 'true'];
+        try {
+            $this->s->setRow($row)->setMonthlyIdToRow(true);
+            $this->fail('例外発生なし');
+        } catch (\Exception $e) {
+            $this->assertEquals("月別IDが指定されていません。", $e->getMessage());
+        }
     }
 
     /**
@@ -55,8 +82,8 @@ class UnitImportZenonDataServiceTest extends TestCase
      */
     public function 正常系_口座番号変換成功() {
         $param = [
-            'account' => 'account_number',
-            'subject' => 'subject_code',
+            'account_column_name' => 'account_number',
+            'subject_column_name' => 'subject_code',
         ];
         $s     = $this->setReflection('convertAccount');
         $res_1 = $s->invoke($this->s, $param, ['account_number' => '1234567890', 'subject_code' => '1',]);
@@ -71,6 +98,139 @@ class UnitImportZenonDataServiceTest extends TestCase
         $this->assertEquals(1234567, $res_4);
         $this->assertEquals(1234567, $res_5);
         $this->assertEquals(1234567890, $res_6);
+    }
+
+    /**
+     * @test
+     */
+    public function 正常系_月別ID指定() {
+        $obj = $this->s->monthlyStatus(201707, [1, 2, 3]);
+    }
+
+    /**
+     * @test
+     */
+    public function 正常系_列セット_分割なし() {
+        $rows        = [
+            ['1', '1234567890', 'key_1', 'test user_1', '20170701', 'true'],
+            ['4', '2345678901', 'key_2', 'test user_2', '2017-08-21', 'false'],
+        ];
+        $timestamp   = date('Y-m-d H:i:s');
+        $expection_1 = [
+            ['subject_code' => 1, 'account_number' => 1234567890.0, 'split_key' => 'key_1', 'user_name' => 'test user_1', 'created_on' => '2017-07-01', 'is_administrator' => true, 'id' => null, 'monthly_id' => 201707, 'key_account_number' => 1234567, 'created_at' => $timestamp, 'updated_at' => $timestamp,],
+            ['subject_code' => 4, 'account_number' => 2345678901.0, 'split_key' => 'key_2', 'user_name' => 'test user_2', 'created_on' => '2017-08-21', 'is_administrator' => false, 'id' => null, 'monthly_id' => 201707, 'key_account_number' => 2345678901, 'created_at' => $timestamp, 'updated_at' => $timestamp,],
+        ];
+        $types       = ['integer', 'double', 'string', 'string', 'date', 'boolean'];
+        $keys        = ['subject_code', 'account_number', 'split_key', 'user_name', 'created_on', 'is_administrator'];
+
+        $res_1 = [];
+        foreach ($rows as $r) {
+            $res_1[] = $this->s->setRow($r)
+                    ->convertRow($types, true)
+                    ->setKeyToRow($keys)
+                    ->setMonthlyIdToRow(true, 201707)
+                    ->setConvertedAccountToRow(true, ['account_column_name' => 'account_number', 'subject_column_name' => 'subject_code'])
+                    ->splitRow(false)
+                    ->setTimeStamp()
+                    ->setTimeStamp($timestamp)
+                    ->getRow()
+            ;
+        }
+//        var_dump($res_1);
+        $this->assertEquals($res_1, $expection_1);
+    }
+
+    /**
+     * @test
+     */
+    public function 正常系_列セット_分割あり() {
+        $rows              = [
+            ['1', '1234567890', 'key_1', 'test user_1', '20170701', 'true'],
+            ['4', '2345678901', 'key_2', 'test user_2', '2017-08-21', 'false'],
+        ];
+        $timestamp         = date('Y-m-d H:i:s');
+        $expection_1       = [
+//            ['subject_code' => 1, 'account_number' => 1234567890.0, 'split_key' => 'key_1', 'user_name' => 'test user_1', 'created_on' => '2017-07-01', 'is_administrator' => true, 'id' => null, 'monthly_id' => 201707, 'key_account_number' => 1234567, 'created_at' => $timestamp, 'updated_at' => $timestamp,],
+//            ['subject_code' => 4, 'account_number' => 2345678901.0, 'split_key' => 'key_2', 'user_name' => 'test user_2', 'created_on' => '2017-08-21', 'is_administrator' => false, 'id' => null, 'monthly_id' => 201707, 'key_account_number' => 2345678901, 'created_at' => $timestamp, 'updated_at' => $timestamp,],
+            ['split_key' => 'key_1', 'user_name' => 'test user_1', 'created_on' => '2017-07-01', 'is_administrator' => true, 'id' => null, 'monthly_id' => 201707, 'key_account_number' => 1234567, 'created_at' => $timestamp, 'updated_at' => $timestamp,],
+            ['split_key' => 'key_2', 'user_name' => 'test user_2', 'created_on' => '2017-08-21', 'is_administrator' => false, 'id' => null, 'monthly_id' => 201707, 'key_account_number' => 2345678901, 'created_at' => $timestamp, 'updated_at' => $timestamp,],
+        ];
+        $types             = ['integer', 'double', 'string', 'string', 'date', 'boolean'];
+        $keys              = ['subject_code', 'account_number', 'split_key', 'user_name', 'created_on', 'is_administrator'];
+        $split_key_configs = ['split_foreign_key_1' => 'split_key', 'split_foreign_key_2' => 'key_account_number'];
+        $convert_keys      = ['account_column_name' => 'account_number', 'subject_column_name' => 'subject_code'];
+
+
+        $res_1 = [];
+        foreach ($rows as $r) {
+            $res_1[] = $this->s->setRow($r)
+                    ->convertRow($types, true)
+                    ->setKeyToRow($keys)
+                    ->setMonthlyIdToRow(true, 201707)
+                    ->setConvertedAccountToRow(true, $convert_keys)
+                    ->splitRow(true, 3, 5, $split_key_configs)
+                    ->setTimeStamp($timestamp)
+                    ->getRow()
+            ;
+        }
+//        var_dump($res_1);
+        $this->assertEquals($res_1, $expection_1);
+    }
+
+    /**
+     * @test
+     */
+    public function 正常系_列セット_変換処理なし() {
+        $rows        = [
+            ['1', '1234567890', 'key_1', 'test user_1', '20170701', 'true'],
+            ['4', '2345678901', 'key_2', 'test user_2', '2017/08/21', 'false'],
+        ];
+//        $timestamp   = date('Y-m-d H:i:s');
+        $expection_1 = [
+            ['subject_code' => 1, 'account_number' => 1234567890.0, 'split_key' => 'key_1', 'user_name' => 'test user_1', 'created_on' => '2017-07-01', 'is_administrator' => true, 'id' => null,],
+            ['subject_code' => 4, 'account_number' => 2345678901.0, 'split_key' => 'key_2', 'user_name' => 'test user_2', 'created_on' => '2017-08-21', 'is_administrator' => false, 'id' => null,],
+        ];
+        $types       = ['integer', 'double', 'string', 'string', 'date', 'boolean'];
+        $keys        = ['subject_code', 'account_number', 'split_key', 'user_name', 'created_on', 'is_administrator'];
+
+        $res_1 = [];
+        foreach ($rows as $r) {
+            $res_1[] = $this->s->setRow($r)
+                    ->convertRow($types, true)
+                    ->setKeyToRow($keys)
+                    ->setMonthlyIdToRow(false)
+                    ->setConvertedAccountToRow(false)
+                    ->getRow()
+            ;
+        }
+//        var_dump($res_1);
+        $this->assertEquals($res_1, $expection_1);
+    }
+
+    /**
+     * @test
+     */
+    public function 異常系_配列分割処理失敗() {
+        $row               = ['1', '1234567890', 'key_1', 'test user_1', '20170701', 'true'];
+        $split_key_configs = ['split_foreign_key_1' => 'split_key', 'split_foreign_key_2' => 'key_account_number'];
+        try {
+            $this->s->setRow($row)->splitRow(true, -1, -1, $split_key_configs);
+            $this->fail('例外発生なし');
+        } catch (\Exception $e) {
+            $this->assertEquals("配列切り落としの開始位置が誤っているようです。", $e->getMessage());
+        }
+        try {
+            $this->s->setRow($row)->splitRow(true, 1, -1, $split_key_configs);
+            $this->fail('例外発生なし');
+        } catch (\Exception $e) {
+            $this->assertEquals("配列切り落としの終了位置が誤っているようです。", $e->getMessage());
+        }
+        try {
+            $this->s->setRow($row)->splitRow(true, 10, 1, $split_key_configs);
+            $this->fail('例外発生なし');
+        } catch (\Exception $e) {
+            $this->assertEquals("配列切り落としの指定は開始位置 < 終了位置となるように指定してください。", $e->getMessage());
+        }
     }
 
 }
