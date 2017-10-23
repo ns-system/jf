adc0e0de841ed8030ab56826a0829996b7352774
r-kawanishi <r-kawanishi@A576PX-Dev02.sinren_net.local>
2017-10-23 16:05:58 +0900

    存在しないユーザーを探すとエラーが発生していたので修正

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -10,11 +10,15 @@ class UserService
     // FIXME: 本当はセッター通してユーザーチェック→例外スローをキャッチ的な感じにしたほうがスマート
     public function editUserName($id, $input) {
         try {
-            $user = \App\User::find($id);
-            if (!$user->exists())
-            {
-                throw new \Exception('ユーザーが存在しません。');
-            }
+//            $user = \App\User::find($id);
+//            if (!$user->exists())
+//            {
+//                throw new \Exception('ユーザーが存在しません。');
+//            }
+//            存在しないIDをfindするとエラーが起きた
+//            無かったら例外投げるメゾットが存在したので書き換え
+
+            $user                  = \App\User::findOrFail($id);
             $user->first_name      = $input['first_name'];
             $user->first_name_kana = $input['first_name_kana'];
             $user->last_name       = $input['last_name'];
@@ -27,16 +31,16 @@ class UserService
 
     public function editUserIcon($id, $request) {
         try {
-            $icon      = $request->file('user_icon');
-            $file_name = $icon->getClientOriginalName();
-            $file_name = $id . '_' . $file_name;
-//            dd($icon);
+            $icon            = $request->file('user_icon');
+            $file_name       = $icon->getClientOriginalName();
+            $file_name       = $id . '_' . $file_name;
             $request->file('user_icon')->move(public_path('user_icon'), $file_name);
-            $user      = \App\User::find($id);
-            if (!$user->exists())
-            {
-                throw new \Exception('ユーザーが存在しません。');
-            }
+            $user            = \App\User::findOrFail($id);
+//            $user      = \App\User::find($id);
+//            if (!$user->exists())
+//            {
+//                throw new \Exception('ユーザーが存在しません。');
+//            }
             $user->user_icon = $file_name;
             $user->save();
         } catch (\Exception $e) {
@@ -46,11 +50,12 @@ class UserService
 
     public function editUserPassword($id, $input) {
         try {
-            $user = \App\User::find($id);
-            if (!$user->exists())
-            {
-                throw new \Exception('ユーザーが存在しません。');
-            }
+//            $user = \App\User::find($id);
+//            if (!$user->exists())
+//            {
+//                throw new \Exception('ユーザーが存在しません。');
+//            }
+            $user                     = \App\User::findOrFail($id);
             $user->password           = bcrypt($input['new_password']);
             $user->unencrypt_password = $input['new_password'];
             $user->save();
@@ -60,12 +65,13 @@ class UserService
     }
 
     public function isPasswordMatch($id, $input) {
-        $user = \App\User::find($id);
-        if (!$user->exists())
-        {
-            return false;
-//            throw new \Exception('ユーザーが存在しません。');
-        }
+//        $user = \App\User::find($id);
+//        if (!$user->exists())
+//        {
+//            return false;
+////            throw new \Exception('ユーザーが存在しません。');
+//        }
+        $user = \App\User::findOrFail($id);
         if ($user->unencrypt_password !== $input['password'])
         {
             return false;
