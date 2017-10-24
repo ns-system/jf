633411d10adebaead4b03fc1cefff08d4849dbad
tosite <tl091264@gmail.com>
2017-10-24 18:07:39 +0900

    ユーザー側UI変更

--- resources/views/roster/app/divisions/list.blade.php
+++ resources/views/roster/app/divisions/list.blade.php
@@ -19,23 +19,24 @@
     <div class="border-bottom">
         <h2>{{$div_name}} <small> - {{$date}}</small></h2>
     </div>
+    <div class="text-right col-md-12">
+        <div data-spy="affix" data-offset="100" style="right: 30px; top: 120px; margin-bottom: 10px;">
+            <div class="btn-group">
+                <a href="{{route('app::roster::division::index', ['div'=>$div])}}" class="btn btn-primary btn-sm" style="min-width: 100px;">
+                    <span class="glyphicon glyphicon-backward" aria-hidden="true"></span> 戻る
+                </a><span></span>
+
+                <a href="{{route('app::roster::division::show', ['div'=>$div,'ym'=>$next])}}" class="btn btn-success btn-sm" style="min-width: 100px;">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> 翌月へ
+                </a><span></span>
+
+                <a href="{{route('app::roster::division::show', ['div'=>$div,'ym'=>$prev])}}" class="btn btn-warning btn-sm" style="min-width: 100px;">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> 前月へ
+                </a>
+            </div>
+        </div>
+    </div>
     @include('roster.app.divisions.partial.list')
-    </div>
-</div>
-<div class="text-right col-md-12">
-    <div class="btn-group">
-        <a href="{{route('app::roster::division::index', ['div'=>$div])}}" class="btn btn-primary" style="min-width: 125px;">
-            <span class="glyphicon glyphicon-backward" aria-hidden="true"></span> 戻る
-        </a><span></span>
-
-        <a href="{{route('app::roster::division::show', ['div'=>$div,'ym'=>$next])}}" class="btn btn-success" style="min-width: 125px;">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> 翌月へ
-        </a><span></span>
-
-        <a href="{{route('app::roster::division::show', ['div'=>$div,'ym'=>$prev])}}" class="btn btn-warning" style="min-width: 125px;">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> 前月へ
-        </a>
-    </div>
 </div>
 @endsection
 
