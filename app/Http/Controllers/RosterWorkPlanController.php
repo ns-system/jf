45bfbce395b978a32b6291312785289f85c69408
r-kawanishi <r-kawanishi@A576PX-Dev02.sinren_net.local>
2017-11-13 14:02:52 +0900

    変数$cntの初期化を追加

--- app/Http/Controllers/RosterWorkPlanController.php
+++ app/Http/Controllers/RosterWorkPlanController.php
@@ -33,6 +33,7 @@ class RosterWorkPlanController extends Controller
 
     public function division($month) {
         $divs  = \App\ControlDivision::user()->get();
+        $cnt =[];
         $users = \App\SinrenUser::join('sinren_db.sinren_divisions', 'sinren_users.division_id', '=', 'sinren_divisions.division_id')
                 ->join('roster_db.roster_users', 'sinren_users.user_id', '=', 'roster_users.user_id')
                 ->join('laravel_db.users', 'sinren_users.user_id', '=', 'users.id')
