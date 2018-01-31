89ad8f00ee100801095486ed06abf59c45cd8148
r-kawanishi <r-kawanishi@A576PX-Dev02.sinren_net.local>
2018-01-31 17:17:27 +0900

    cir_created_onが消失していた現象の修正

--- database/migrations/2018_01_26_110109_chang_customer_information_files.php
+++ database/migrations/2018_01_26_110109_chang_customer_information_files.php
@@ -5,7 +5,8 @@ use Illuminate\Database\Migrations\Migration;
 
 class ChangCustomerInformationFiles extends Migration
 {
-     public $tableName = 'customer_information_files';
+
+    public $tableName = 'customer_information_files';
     public $connect   = 'mysql_zenon';
 
     public function up() {
@@ -14,18 +15,11 @@ class ChangCustomerInformationFiles extends Migration
             {
                 $table->dropColumn('cir_created_on');
             }
-            if (!Schema::connection($this->connect)->hasColumn($this->tableName, 'cir_created_on'))
-            {
-                $table->date("cir_created_on")
-                        ->nullable()
-                        ->after('cir_number');
-
-
-            }
         });
     }
 
     public function down() {
-       
+        
     }
+
 }
