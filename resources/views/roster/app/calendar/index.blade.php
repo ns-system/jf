69c8d27cfa53df94d158785df75c6b2e22db36c4
tosite <tl091264@gmail.com>
2018-02-22 16:01:13 +0900

    R_10_xx フロントエンド修正

--- resources/views/roster/app/calendar/index.blade.php
+++ resources/views/roster/app/calendar/index.blade.php
@@ -41,7 +41,11 @@
             <div class="well well-sm">
                 <small>
                     <p>責任者が作成した勤務予定データに対して実績を入力してください。</p>
-                    <p>この画面から残業申請を上げることもできます。</p>
+                    <ul>
+                        <li>残業申請を行う場合、「申請」ボタンから申請を行ってください。</li>
+                        <li>残業を行わない場合も「申請」ボタンから更新を行ってください。この場合、内容を修正する必要はありません。</li>
+                        <li>一度承認されたデータは修正することができません。</li>
+                    </ul>
                 </small>
             </div>
             
@@ -93,24 +97,22 @@
                                     
                                     data-toggle="modal"
                                     data-target="#plan-{{$r->id}}"
+                                    class="btn btn-primary btn-xs"
                                     @if(!empty($r->is_plan_entry))
-                                    @if($r->is_plan_accept)     class="btn btn-success btn-xs" disabled
-                                    @elseif($r->is_plan_reject) class="btn btn-danger  btn-xs"         
-                                    @else                       class="btn btn-warning btn-xs"         @endif
-                                    @else class="btn btn-primary btn-xs"@endif
-                                    ><span>予定</span>
+                                    @if($r->is_plan_accept) disabled @endif
+                                    @endif
+                                    ><span>申請</span>
                                 </button>
                                 <button
                                 type="button"
+                                class="btn btn-xs btn-primary"
                                 @if(!empty($r->is_plan_entry))
                                 data-toggle="modal"
                                 data-target="#actual-{{$r->id}}"
                                 @if(!empty($r->is_actual_entry))
-                                @if($r->is_actual_accept)     class="btn btn-success btn-xs" disabled
-                                @elseif($r->is_actual_reject) class="btn btn-danger  btn-xs"          
-                                @else                         class="btn btn-warning btn-xs"          @endif
-                                @else class="btn btn-primary btn-xs" @endif
-                                @else class="btn btn-primary btn-xs" onclick="alert('先に予定を登録してください。');"
+                                @if($r->is_actual_accept) disabled @endif
+                                @endif
+                                @else onclick="alert('先に予定を登録してください。');"
                                 @endif
                                 ><span>実績</span>
                             </button>
@@ -123,7 +125,7 @@
                             @else                                               onclick="return confirm('予定・実績データが削除されますが本当によろしいですか？');" @endif
                             ><span class="glyphicon glyphicon-trash" aria-hidden="true"></span>　</a>
                             @else
-                            <button type="button" class="btn btn-primary btn-xs" disabled>予定</button>
+                            <button type="button" class="btn btn-primary btn-xs" disabled>申請</button>
                             <button type="button" class="btn btn-primary btn-xs" disabled>実績</button>
                             <button type="button" class="btn btn-primary btn-xs" disabled>　<span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
                             @endif
@@ -147,37 +149,47 @@
                     <div class="text-left">
                         {{-- Plan --}}
                         @if($r->is_plan_entry)
-                        @if($r->is_plan_reject)     <span class="label label-danger">予定</span>
-                        @elseif($r->is_plan_accept) <span class="label label-success">予定</span>
-                        @else                       <span class="label label-warning">予定</span> @endif
-                        @else <span class="label label-default">予定</span> @endif
-                        @if(!empty($r->plan_work_type_id))      <p class="small">{{$types[$r->plan_work_type_id]['name']}} {{$types[$r->plan_work_type_id]['time']}}</p> @endif
-                        @if(!empty($r->plan_overtime_start_time) &&
-                        !empty($r->plan_overtime_end_time))     <p class="small">{{date('G:i', strtotime($r->plan_overtime_start_time))}} ～ {{date('G:i', strtotime($r->plan_overtime_end_time))}}</p> @endif
-                        @if(!empty($r->plan_rest_reason_id))        <p class="small">{{$rests[$r->plan_rest_reason_id]}}</p> @endif
-                        @if(!empty($r->plan_overtime_reason))       <p class="small">{{$r->plan_overtime_reason}}</p> @endif
+                        <span data-toggle="tooltip" data-placement="top" @if(!empty($r->plan_accept_user_id)) title="{{ \App\User::find($r->plan_accept_user_id)->last_name }}さん" @endif @if(!empty($r->plan_reject_user_id)) title="{{ \App\User::find($r->plan_reject_user_id)->last_name }}さん @if(!empty($r->reject_reason)) ／ {{ $r->reject_reason }} @endif" @endif>
 
-                        {{-- Actual --}}
-                        @if($r->is_actual_entry)
+                        @if($r->is_plan_reject)     <span class="label label-danger">申請</span>
+                        @elseif($r->is_plan_accept) <span class="label label-success">申請</span>
+                        @else                       <span class="label label-warning">申請</span>
+                        @endif
+                    </span>
+
+                    @else <span><span class="label label-default">申請</span></span> @endif
+                    @if(!empty($r->plan_work_type_id))      <p class="small">{{$types[$r->plan_work_type_id]['name']}} {{$types[$r->plan_work_type_id]['time']}}</p> @endif
+                    @if(!empty($r->plan_overtime_start_time) &&
+                    !empty($r->plan_overtime_end_time))     <p class="small">{{date('G:i', strtotime($r->plan_overtime_start_time))}} ～ {{date('G:i', strtotime($r->plan_overtime_end_time))}}</p> @endif
+                    @if(!empty($r->plan_rest_reason_id))        <p class="small">{{$rests[$r->plan_rest_reason_id]}}</p> @endif
+                    @if(!empty($r->plan_overtime_reason))       <p class="small">{{$r->plan_overtime_reason}}</p> @endif
+
+                    {{-- Actual --}}
+                    @if($r->is_actual_entry)
+                    <span data-toggle="tooltip" data-placement="top" @if(!empty($r->actual_accept_user_id)) title="{{ \App\User::find($r->actual_accept_user_id)->last_name }}さん" @endif @if(!empty($r->actual_reject_user_id)) title="{{ \App\User::find($r->actual_reject_user_id)->last_name }}さん @if(!empty($r->reject_reason)) ／ {{ $r->reject_reason }} @endif" @endif>
+
+
                         @if($r->is_actual_reject)     <span class="label label-danger">実績</span>
                         @elseif($r->is_actual_accept) <span class="label label-success">実績</span>
                         @else                         <span class="label label-warning">実績</span> @endif
-                        @if(!empty($r->actual_work_type_id))      <p class="small">{{$types[$r->actual_work_type_id]['name']}} {{$types[$r->actual_work_type_id]['time']}}</p> @endif
-                        @if(!empty($r->actual_overtime_start_time) &&
-                        !empty($r->actual_overtime_end_time))     <p class="small">{{date('G:i',strtotime($r->actual_overtime_start_time))}} ～ {{date('G:i', strtotime($r->actual_overtime_end_time))}}</p> @endif
-                        @if(!empty($r->actual_rest_reason_id))        <p class="small">{{$rests[$r->actual_rest_reason_id]}}</p> @endif
-                        @if(!empty($r->actual_overtime_reason))       <p class="small">{{$r->actual_overtime_reason}}</p> @endif
-                        @endif
-                    </div>
+                    </span>
+                    
+                    @if(!empty($r->actual_work_type_id))      <p class="small">{{$types[$r->actual_work_type_id]['name']}} {{$types[$r->actual_work_type_id]['time']}}</p> @endif
+                    @if(!empty($r->actual_overtime_start_time) &&
+                    !empty($r->actual_overtime_end_time))     <p class="small">{{date('G:i',strtotime($r->actual_overtime_start_time))}} ～ {{date('G:i', strtotime($r->actual_overtime_end_time))}}</p> @endif
+                    @if(!empty($r->actual_rest_reason_id))        <p class="small">{{$rests[$r->actual_rest_reason_id]}}</p> @endif
+                    @if(!empty($r->actual_overtime_reason))       <p class="small">{{$r->actual_overtime_reason}}</p> @endif
                     @endif
-                    {{-- パネル内容 --}}
                 </div>
+                @endif
+                {{-- パネル内容 --}}
             </div>
-            <?php unset($r); ?>
-            @endif
-        </td>
-    @if($day['week'] == 6) </tr> @endif
-    @endforeach
+        </div>
+        <?php unset($r); ?>
+        @endif
+    </td>
+@if($day['week'] == 6) </tr> @endif
+@endforeach
 </tbody>
 </table>
 
