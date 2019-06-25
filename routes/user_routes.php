e16183a1c5b3c0be95c0677ff44664df6445f650
erugis0345 <tl091264@gmail.com>
2019-06-26 00:30:14 +0900

    new routes

---
+++ routes/user_routes.php
@@ -0,0 +1,28 @@
+<?php
+
+// ===========================================================================================================================
+/**
+ * Role       : auth
+ * Middleware : auth
+ * Prefix     : /app
+ * As         : app::
+ */
+Route::group(['middleware' => 'auth', 'prefix' => '/app', 'as' => 'app::'], function() {
+    /**
+     * Role       : auth
+     * Prefix     : /user
+     * As         : user::
+     */
+    Route::group(['prefix' => '/user', 'as' => 'user::'], function() {
+        Route::get('/{id}',           ['as' => 'show', 'uses' => 'UserController@show']);
+        Route::post('/name/{id}',     ['as' => 'name', 'uses' => 'UserController@name']);
+        Route::post('/icon/{id}',     ['as' => 'icon', 'uses' => 'UserController@userIcon']);
+        Route::post('/division/{id}', ['as' => 'division', 'uses' => 'UserController@division']);
+        Route::post('/password/{id}', ['as' => 'password', 'uses' => 'UserController@password']);
+
+        Route::group(['prefix' => '/{id}', 'as' => 'font::'], function() {
+            Route::get('/fonts',         ['as' => 'show', 'uses' => 'FontController@index']);
+            Route::post('/fonts/update', ['as' => 'update', 'uses' => 'FontController@update']);
+        });
+    });
+});
